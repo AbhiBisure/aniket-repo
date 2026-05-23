d54e62403cf1f76c10d216336dee5f8685a10245
AbhiBisure <abhijeetbisure261@gmail.com>
2026-05-23 22:17:06 +0530

    fs file commited

--- express_routes/index.js
+++ express_routes/index.js
@@ -1,32 +1,40 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// app.use(express.json());
+const app = express();
+app.use(express.json());
 
-// let users = [];
+let users = [];
 
-// app.post("/users", (req, res) => {
-//   users.push(req.body);
-//   res.status(201).json({
-//     message: "User Added",
-//   });
-// });
-
-// app.get("/users", (req, res) => {
-//   res.json(users);
-// });
-
-// app.listen(3000);
-
-
-const fs = require("fs")
-
-fs.readFile("text.txt","utf8",(err,data)=>{
-    if(err)
-    {
-        console.log(err);
-        return;
-    }
-
-    console.log(data);
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.status(201).json({
+    message: "User Added",
+  });
 });
+
+app.post("/addUsers",(req,res)=>{
+    res.send("user added")
+    users.push(req.body)
+    res.status(200).json({
+        message:"new user added"
+    })
+})
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(3000);
+
+
+// const fs = require("fs")
+
+// fs.readFile("text.txt","utf8",(err,data)=>{
+//     if(err)
+//     {
+//         console.log(err);
+//         return;
+//     }
+
+//     console.log(data);
+// });
